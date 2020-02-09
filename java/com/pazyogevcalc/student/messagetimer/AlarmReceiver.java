32bdf14940e92a8459cbbbe1aa5cd3b88557d3fe
udixd <60843337+udixd@users.noreply.github.com>
2020-02-09 15:31:18 +0200

    Update AlarmReceiver.java

--- java/com/pazyogevcalc/student/messagetimer/AlarmReceiver.java
+++ java/com/pazyogevcalc/student/messagetimer/AlarmReceiver.java
@@ -7,20 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-public class AlarmReceiver extends AppCompatActivity {
+public class AlarmReceiver extends BroadcastReceiver {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm_receiver);
-    }
-
-
     public void onReceive(Context context, Intent intent) {
-//you can check the log that it is fired
-//Here we are actually not doing anything
-//but you can do any task here that you want to be done at a specific time everyday
-// For our recurring task, we'll just display a message
         Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
         Log.d("MyAlarmBelal", "Alarm just fired");
     }
